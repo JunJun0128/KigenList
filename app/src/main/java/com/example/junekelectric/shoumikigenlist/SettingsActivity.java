49d9a995938069fc9ad1655c3c9a871019a9e39d
JunJun0128 <june.kishimoto@gmail.com>
2017-06-23 20:17:14 +0900

    2017/06/23設定に変更

--- app/src/main/java/com/example/junekelectric/shoumikigenlist/SettingsActivity.java
+++ app/src/main/java/com/example/junekelectric/shoumikigenlist/SettingsActivity.java
@@ -1,8 +1,24 @@
 package com.example.junekelectric.shoumikigenlist;
 
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.WindowManager;
+
 /**
  * Created by junekelectric on 2017/06/09.
  */
 
-public class SettingsActivity {
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        FragmentTransaction fragmentTransaction =
+                getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(android.R.id.content,
+                new SettingsPreferenceFragment());
+        fragmentTransaction.commit();
+    }
 }
--- app/src/main/java/com/example/junekelectric/shoumikigenlist/SettingsPreferenceFragment.java
+++ app/src/main/java/com/example/junekelectric/shoumikigenlist/SettingsPreferenceFragment.java
@@ -1,9 +1,17 @@
 package com.example.junekelectric.shoumikigenlist;
 
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
 /**
  * Created by junekelectric on 2017/06/09.
  */
 
-public class SettingsPreferenceFragment {
-
+public class SettingsPreferenceFragment extends PreferenceFragment {
+    //設定画面を表示するクラス
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.activity_settings);
+    }
 }
